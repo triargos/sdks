c65a3981328ad708cb7bb690d75d58db565ba680
Tim Zolleis <tim@zolleis.net>
2024-07-07 15:35:21 +0200

    fix(dashboard): use correct header

--- packages/dashboard-node/src/dashboard.ts
+++ packages/dashboard-node/src/dashboard.ts
@@ -1,27 +1,27 @@
-import { GeneratedDashboard } from './client';
+import {GeneratedDashboard} from './client';
 
 type Environment = 'staging' | 'production' | 'development';
 
 export const PRODUCTION_URL = 'https://dashboard.triargos.de/api';
-export const STAGING_URL = 'https://staging.dashboard.triargos.de/api';
+export const STAGING_URL = 'https://dashboard.staging.triargos.de/api';
 export const DEVELOPMENT_URL = 'http://localhost:3000/api';
 
 export class Dashboard extends GeneratedDashboard {
-  constructor({
-    apiKey,
-    environment = 'production',
-  }: {
-    apiKey: string;
-    environment?: Environment;
-  }) {
-    super({
-      BASE:
-        environment === 'production'
-          ? PRODUCTION_URL
-          : environment === 'staging'
-            ? STAGING_URL
-            : DEVELOPMENT_URL,
-      HEADERS: { 'Content-Type': 'application/json', 'X-Api-Key': apiKey },
-    });
-  }
+    constructor({
+                    apiKey,
+                    environment = 'production',
+                }: {
+        apiKey: string;
+        environment?: Environment;
+    }) {
+        super({
+            BASE:
+                environment === 'production'
+                    ? PRODUCTION_URL
+                    : environment === 'staging'
+                        ? STAGING_URL
+                        : DEVELOPMENT_URL,
+            HEADERS: {'Content-Type': 'application/json', 'Authorization': `Bearer ${apiKey}`},
+        });
+    }
 }

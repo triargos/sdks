48225e7c23a6352801465b110dc4bd565e939f3a
Tim Zolleis <tim@zolleis.net>
2024-08-02 09:28:10 +0200

    BREAKING CHANGE: remove openapi ts

--- packages/dashboard-node/openapi-ts.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from '@hey-api/openapi-ts';
-
-export default defineConfig({
-  input: 'openapi.json',
-  output: 'src/client',
-  client: 'axios',
-  name: 'GeneratedDashboard',
-});
